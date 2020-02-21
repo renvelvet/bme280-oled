a8ea3af7a12c847a32f312ff23f1a6387d1566ab
Resha Puspita <reshapuspita734@gmail.com>
2020-02-21 13:14:56 +0700

    Update and rename bme280-oled to bme280-oled.ino

--- bme280-oled.ino
+++ bme280-oled.ino
@@ -7,7 +7,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_Sensor.h>
-//#include <DHT.h>
 #include <Adafruit_BME280.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -18,18 +17,10 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 Adafruit_BME280 bme; // I2C
 
-//#define DHTPIN 14     // Digital pin connected to the DHT sensor
-//
-//// Uncomment the type of sensor in use:
-////#define DHTTYPE    DHT11     // DHT 11
-//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
-////#define DHTTYPE    DHT21     // DHT 21 (AM2301)
-//
-//DHT dht(DHTPIN, DHTTYPE);
-
 void setup() {
   Serial.begin(115200);
 
+  // inisialisasi alamat bme280
   bme.begin(0x76);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
